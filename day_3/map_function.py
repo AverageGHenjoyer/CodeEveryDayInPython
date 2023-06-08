46fd15f6c0f93e7fbe75d9f84659059b9f09ed16
AverageGHenjoyer <maciej.ko1444@gmail.com>
2023-06-08 10:02:57 +0200

    Second print with the map class is getting zeroed out :O

--- day_3/map_function.py
+++ day_3/map_function.py
@@ -1,4 +1,5 @@
 # map function
+# syntax: map(function, iterable)
 # 1.
 mylist = []
 for x in range(1, 100):
@@ -10,11 +11,13 @@ print(list(new_list))
 
 # 2.
 
-my_map = map(lambda x1, x2: x1 % x2, (10, 11, 12), [1, 2, 3])
+my_map = map(lambda x1, x2: x1 % x2, (10, 11, 12), [1, 2, 3])  # different data types and still works fine
 print(my_map)
 # print result: <map object at ...>
 # Map class is lazily evaluated.
 # It means that
 # I need to convert it into list/tuple/set.
 print(list(my_map))
-print(list(my_map))  # empty list :O
+print(tuple(my_map))  # empty tuple :O
+#  Second print and map gets zeroed out
+#  I guess that it is intended to be used once.
