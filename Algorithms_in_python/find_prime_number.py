f314f83547c982422260b967e8ac17841579f93d
AverageGHenjoyer <maciej.ko1444@gmail.com>
2023-06-13 11:14:00 +0200

    Bugs were detected, code has been changed, everything is now working fine!

--- Algorithms_in_python/find_prime_number.py
+++ Algorithms_in_python/find_prime_number.py
@@ -1,17 +1,23 @@
 # 13.06.2023
 # Test if given number is a prime number
-user_input = 0
-while user_input < 2:
+while True:
     try:
         user_input = int(input("Enter a number, so I will check if it is a prime number \n"))
     except ValueError:
         print("Invalid input. Sorry for inconvenience. Please try again.")
-
-
-for x in range(2, user_input):
-    if user_input % x == 0:
-        print("It is not a prime number... :/")
+    else:
         break
+if user_input < 2:
+    print("Invalid number. Please try again.")
+elif user_input > 1:
+    for x in range(2, user_input):
+        if (user_input % x) == 0:
+            print(f"{user_input} is not a prime number... :/")
+            print(f"Found divider: {x}")
+            break
     else:
         print("It is a prime number! :O")
-        break
+
+
+else:
+    print(user_input,"is not a prime number")
