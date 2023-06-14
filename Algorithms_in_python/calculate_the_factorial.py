f0a45e636b85cabe51a6668de1756032f4ab8bcf
AverageGHenjoyer <maciej.ko1444@gmail.com>
2023-06-14 19:44:37 +0200

    Second way to do that added.

--- Algorithms_in_python/calculate_the_factorial.py
+++ Algorithms_in_python/calculate_the_factorial.py
@@ -1,17 +1,33 @@
-def calc_the_factorial(x):
+#
+# def calc_the_factorial(x:int) -> int:
+#     if x == 0:
+#         return 1
+#     else:
+#         return x*calc_the_factorial(x-1)
+#
+#
+# while True:
+#     try:
+#         user_input = int(input("Give me a number: \n"))
+#     except ValueError:
+#         print("Something has gone wrong. Please try again.")
+#     else:
+#         print(f"Calculated factorial: {calc_the_factorial(user_input)}")
+#         break
+
+# 2.
+def factorial(x, tmp=1):
     if x == 0:
-        return 1
+        return tmp
     else:
-        return x*calc_the_factorial(x-1)
+        return factorial(x-1, tmp*x)
 
 
 while True:
     try:
-        user_input = int(input("Give me a number: \n"))
+        given_number = int(input("Give me a number: \n"))
     except ValueError:
         print("Something has gone wrong. Please try again.")
     else:
-        print(f"Calculated factorial: {calc_the_factorial(user_input)}")
+        print(f"Calculated factorial: {factorial(given_number)}")
         break
-
-
